baf151094c4160152a28d5debd90f649010adc9b
aziz <a.gharbi@mphase.ca>
2020-07-17 02:06:22 -0400

    rename

---
+++ todo/Components/TaskInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { SafeAreaView, TextInput, Button } from "react-native";
+import { styles } from "../styles";
+
+export default TaskInput = ({ tasks, setTask }) => {
+  const [value, onChangeText] = useState("");
+
+  function addTask() {
+    setTask([
+      ...tasks,
+      {
+        id: new Date(),
+        title: value,
+      },
+    ]);
+  }
+
+  return (
+    <SafeAreaView style={styles.inputContainer}>
+      <TextInput
+        placeholder="Enter your task"
+        style={styles.input}
+        onChangeText={(text) => onChangeText(text)}
+        value={value}
+      />
+      <Button color="#f9c2ff" title="add task" onPress={addTask}></Button>
+    </SafeAreaView>
+  );
+};
